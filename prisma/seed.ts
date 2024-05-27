64bd54f6349f58e57f51c7f7e803a2fe69c22ba7
ng <ng_zi_jie@hotmail.com>
2024-05-27 21:41:46 +0800

    added name to product table and completed seed for users. 5 users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,8 +4,10 @@ const prisma = new PrismaClient()
 
 async function main (){
 
+  const deletedProductInfo = await prisma.Product.deleteMany();
   const deletedUserInfo = await prisma.User.deleteMany();
 
+  console.log("Deleted all products")
   console.log("Deleted all users")
 
   const password1 = await hash("password1", 10)
@@ -29,9 +31,45 @@ async function main (){
       active: true
     },
   });
-  
+
+  const password3 = await hash("password3", 10)
+  const user3 = await prisma.User.create({
+    data: {
+      name: 'Melvin',
+      email: "usertest3@email.com",
+      provider: "Credentials",
+      password: password3,
+      active: true
+    },
+  });
+
+  const password4 = await hash("password4", 10)
+  const user4 = await prisma.User.create({
+    data: {
+      name: 'Limzy',
+      email: "usertest4@email.com",
+      provider: "Credentials",
+      password: password4,
+      active: true
+    },
+  });
+
+  const password5 = await hash("password5", 10)
+  const user5 = await prisma.User.create({
+    data: {
+      name: 'Kelvin Yap',
+      email: "usertest5@email.com",
+      provider: "Credentials",
+      password: password4,
+      active: true
+    },
+  });
+
   console.log("Created user1: ", user1);
   console.log("Created user2: ", user2);
+  console.log("Created user3: ", user3);
+  console.log("Created user4: ", user4);
+  console.log("Created user5: ", user5);
 
 }
 
