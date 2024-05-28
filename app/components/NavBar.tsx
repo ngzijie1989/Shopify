e39b92ae8592bc08e280c0f5045ba1624bedb2eb
ng <ng_zi_jie@hotmail.com>
2024-05-28 20:49:51 +0800

    save navbar changes

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { IoCartOutline } from "react-icons/io5";
 import { MdFavoriteBorder } from "react-icons/md";
 import { MdCurrencyExchange } from "react-icons/md";
+import { FaRegUserCircle } from "react-icons/fa";
 import Link from "next/link"
 
 function NavBar() {
@@ -18,6 +19,23 @@ function NavBar() {
       <button className="btn text-xl"><MdCurrencyExchange /></button>
       <Link href="/favorites" className="btn btn-ghost text-xl"><MdFavoriteBorder /></Link>
       <Link href="/cart" className="btn btn-ghost text-xl"><IoCartOutline /></Link>
+      <Link href="/cart" className="btn btn-ghost text-xl"><FaRegUserCircle /></Link>
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button">
+          <div className="btn text-xl">
+            <FaRegUserCircle />
+          </div>
+        </div>
+        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <li>
+            <a className="justify-between">
+              Profile
+            </a>
+          </li>
+          <li><a>Settings</a></li>
+          <li><a>Logout</a></li>
+        </ul>
+      </div>
     </div>
   </div>
   )
