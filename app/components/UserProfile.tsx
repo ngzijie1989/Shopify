fac013f3fca6e9c4b687fead2c31249223dbe4d8
ng <ng_zi_jie@hotmail.com>
2024-06-22 21:07:49 +0800

    adjust profile css

--- app/components/UserProfile.tsx
+++ app/components/UserProfile.tsx
@@ -1,12 +1,15 @@
 import { UserType } from "../lib/type"
+import styles from "@/app/lib/css/login.module.css"
 
 function UserProfile({user}: {user: UserType}) {
 
   return (
-    <div className="w-2/5 mx-auto my-5 text-center bg-slate-300 rounded p-3">
-      <h1 className="font-bold text-xl sm:text-2xl">Your Profile</h1>
-      <p className="text-xs sm:text-sm md:text-base">Email: {user.email}</p>
-      <p className="text-xs sm:text-sm md:text-base">Name: {user.name}</p>
+    <div className={`${styles.profileContainer}`}>
+      <div className="w-2/5 mx-auto my-5 text-center bg-slate-300 rounded-lg p-3">
+        <h1 className="font-bold text-xl sm:text-2xl">Your Profile</h1>
+        <p className="text-xs sm:text-sm md:text-base">Email: {user.email}</p>
+        <p className="text-xs sm:text-sm md:text-base">Name: {user.name}</p>
+      </div>
     </div>
   )
 }
