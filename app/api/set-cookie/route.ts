5ed9f20b1a9d511856ce7b49a1ad5495b5886b91
ng <ng_zi_jie@hotmail.com>
2024-06-22 13:32:02 +0800

    remove redundant api routes

--- app/api/set-cookie/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-import { setServerCookie } from "@/app/lib/utils/cookie"
-
-export async function POST(info) {
-  const set = await setServerCookie()
-  console.log(set)
-      
-  return NextResponse.json(set)
-}
