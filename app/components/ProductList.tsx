4b6dc7e5ec4cb94f714d6d59ee75f3717b3049ae
ng <ng_zi_jie@hotmail.com>
2024-06-22 16:08:12 +0800

    edit favico and async await function within client

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -1,7 +1,7 @@
 import { ProductType } from "../lib/type"
 import Product from "./Product"
 
-async function ProductList({products, session, favProductsIds}: {products: ProductType[], session: any, favProductsIds: string[]}) {
+function ProductList({products, session, favProductsIds}: {products: ProductType[], session: any, favProductsIds: string[]}) {
   
   return (
     <div className="grid grid-cols-2 gap-6 w-full p-3 mx-auto sm:grid-cols-3 md:grid-cols-4">
