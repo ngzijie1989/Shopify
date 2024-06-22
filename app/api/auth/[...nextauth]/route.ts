1cd8ed49993c5ee3dd8757bb32da542dc94cc574
ng <ng_zi_jie@hotmail.com>
2024-06-22 13:29:24 +0800

    try auth change ruote

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,10 @@
 import { handlers, signIn as signInFunction, signOut } from "@/auth";
 
-// export const { GET, POST } = handlers 
+export const { GET, POST } = handlers 
 // export const signIn = signInFunction;
 
-export const combinedExports = {
-  GET: handlers.GET,
-  POST: handlers.POST,
-  signIn: signInFunction
-};
+// export const combinedExports = {
+//   GET: handlers.GET,
+//   POST: handlers.POST,
+//   signIn: signInFunction
+// };
