0bbd643625f69c7a411a4adfa9697c0333d5c647
ng <ng_zi_jie@hotmail.com>
2024-06-22 13:26:16 +0800

    edit auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,10 @@
 import { handlers, signIn as signInFunction, signOut } from "@/auth";
 
-export const { GET, POST } = handlers 
-export const signIn = signInFunction;
+// export const { GET, POST } = handlers 
+// export const signIn = signInFunction;
+
+export const combinedExports = {
+  GET: handlers.GET,
+  POST: handlers.POST,
+  signIn: signInFunction
+};
